d5354d59a36c023d9b16a97bb15988276b1a117c
Tinyu Liu <tinyu1016@gmail.com>
2019-11-26 15:19:19 +0000

    Assignment1-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,16 +1,94 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { nullLiteral } from '@babel/types';
+
 
 class Dishdetail extends Component{
+
     constructor(props){
         super(props);
 
-        this.state ={
-            delectedDish: null
-        }
     }
+        monthf(m){
+            var month = new Array();
+            month['01'] = "Jan";
+            month['02'] = "Feb";
+            month['03'] = "Mar";
+            month['04'] = "Api";
+            month['05'] = "May";
+            month['06'] = "Jun";
+            month['07'] = "Jul";
+            month['08'] = "Agu";
+            month['09'] = "Sep";
+            month['10'] = "Oct";
+            month['11'] = "Nov";
+            month['12'] = "Dec";
+        
+            return month[m]
+        }
+        
+
+        renderComments(dish){
+
+            if (dish!=null){
+                console.log(dish.comments[1].author);
+               
+                    return(
+                    
+                        <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            
+                            {dish.comments.map((value, index) =>{
+                            return [   
+                                <p key={index}> {value.comment}</p>,
+                            <p key={index}>--{value.author},{this.monthf(value.date.substr(5,2))}, {value.date.substr(8,2)}, {value.date.substr(0,4)}</p>                            
+                            ]})}
+                        </div>
+                    );
+               
+            }
+            else{
+                return(
+                    <div className="col-12 col-md-5 m-1">
+    
+                    </div>
+                );
+            }
+        }
+           
+        renderDish(dish){
+            if (dish !=null){
+                return(
+                    <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                    </div>
+                );
+            }
+            else{
+                return(
+                    <div className="col-12 col-md-5 m-1">
+    
+                    </div>
+                );
+            }
+        }
+
+        render(){
+            // const dish = this.props.selectedDish
+            return(
+                <div className="row">
+                        {this.renderDish(this.props.selectedDish)}
+                        {this.renderComments(this.props.selectedDish)}
+
+                </div>
+            );
+        }
 }
 
-onDishSelect(dish){
-    this.SVGElementInstanceList({selectedDish:dish});
-}
+export default Dishdetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import Dishdetail from './DishdetailComponent';
 class Menu extends Component{
     constructor(props){
         super(props);
@@ -17,78 +17,6 @@ class Menu extends Component{
 
     onDishSelect(dish){
         this.setState({selectedDish:dish});
-    }
-
-    monthf(m){
-        var month = new Array();
-        month['01'] = "Jan";
-        month['02'] = "Feb";
-        month['03'] = "Mar";
-        month['04'] = "Api";
-        month['05'] = "May";
-        month['06'] = "Jun";
-        month['07'] = "Jul";
-        month['08'] = "Agu";
-        month['09'] = "Sep";
-        month['10'] = "Oct";
-        month['11'] = "Nov";
-        month['12'] = "Dec";
-
-        return month[m]
-    }
-    
-
-    renderComments(dish){
-
-        if (dish!=null){
-            console.log(dish.comments[1].author);
-           
-                return(
-                
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        
-                        {dish.comments.map((value, index) =>{
-                        return [   
-                            <p key={index}> {value.comment}</p>,
-                        <p key={index}>--{value.author},{this.monthf(value.date.substr(5,2))}, {value.date.substr(8,2)}, {value.date.substr(0,4)}</p>                            
-                        ]})}
-                    </div>
-                );
-           
-        }
-        else{
-            return(
-                <div className="col-12 col-md-5 m-1">
-
-                </div>
-            );
-        }
-    }
-
-
-    
-    renderDish(dish){
-        if (dish !=null){
-            return(
-                <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-                </div>
-            );
-        }
-        else{
-            return(
-                <div className="col-12 col-md-5 m-1">
-
-                </div>
-            );
-        }
     }
 
     render(){
@@ -116,10 +44,7 @@ class Menu extends Component{
                 <div className="row">
                         {menu}
                 </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                    {this.renderComments(this.state.selectedDish)}
-                </div>
+                <Dishdetail selectedDish={this.state.selectedDish}/>
             </div>
         );
     }
